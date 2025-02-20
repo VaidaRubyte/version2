2058d74b07c500b23dde9be6800003f318ff57c5
VaidaRubyte <73707561+VaidaRubyte@users.noreply.github.com>
2025-02-20 22:26:18 +0000

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2171852.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 214339860, 214339862, 214339885, 214339865, 214339866, 214339867, 214339868, 214339869, 214339871, 214339872, 214339873, 214339874, 214339876, 214339877, 214516469, 214932035, 214960127 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/214339882/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/214339883/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/214339881/1/ammo.js', 'preload' : true},
+];
